658cc7680128ca7324e4ba23d89c63bad47a1fe9
JuliaKravchenko8 <107355879+JuliaKravchenko8@users.noreply.github.com>
2022-10-23 00:04:36 +0300

    task-01 in progress

---
+++ js/task-01.js
@@ -0,0 +1,6 @@
+const categories = document.querySelectorAll("li.item");
+console.log("Number of categories: ", categories.length);
+const categoryTitle = document.querySelector("h2");
+console.log("Category: ", categoryTitle.textContent);
+const elements = document.querySelector("li");
+console.log(elements.length);
