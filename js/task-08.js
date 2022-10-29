f236869208ad451b7f0f1d0bd3799e48e321b77b
JuliaKravchenko8 <107355879+JuliaKravchenko8@users.noreply.github.com>
2022-10-29 23:12:09 +0300

    task-08 finish

---
+++ js/task-08.js
@@ -0,0 +1,18 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені!");
+  }
+  const formElements = { mail: email.value, password: password.value };
+
+  console.log(formElements);
+
+  event.currentTarget.reset;
+}
