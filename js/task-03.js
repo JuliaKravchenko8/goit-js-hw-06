8aa5c1aa12ac03f6091195cf257dbb472ab94b13
JuliaKravchenko8 <107355879+JuliaKravchenko8@users.noreply.github.com>
2022-11-01 00:04:19 +0200

    update task-03

--- js/task-03.js
+++ js/task-03.js
@@ -1,28 +1,28 @@
 const images = [
   {
-    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
-    alt: "White and Black Long Fur Cat",
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
   },
   {
-    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
-    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
-    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
-    alt: "Group of Horses Running",
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
   },
 ];
 
-const addImg = document.querySelector(".gallery");
+const galleryEl = document.querySelector('.gallery');
+const imagesEl = images
+  .map(
+    item =>
+      `<li class = "gallery__item"><img class = "gallery__img", src = "${item.url}", alt = "${item.alt}", width = '200px', heigth = "100px" /></li>`
+  )
+  .join('');
 
-images.forEach((img) => {
-  addImg.insertAdjacentHTML(
-    "afterbegin",
-    `<li><img src="${img.url}" alt="${img.alt}" width = '200' height = '150'></li>`
-  );
-  addImg.setAttribute(
-    "style",
-    "list-style:none; display: flex; gap: 15px; padding-top: 30px; justify-content:center;"
-  );
-});
-console.log(addImg);
+galleryEl.insertAdjacentHTML('afterbegin', imagesEl);
+galleryEl.setAttribute(
+  'style',
+  'list-style:none; display: flex; flex-direction: column; padding-top: 30px;'
+);
